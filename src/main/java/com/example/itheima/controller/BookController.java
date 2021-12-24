4f437ed5c1d8c70f00523c767968a179861acf34
gzh <936285702@qq.com>
2021-12-24 17:41:14 +0800

    添加错误拦截

--- src/main/java/com/example/itheima/controller/BookController.java
+++ src/main/java/com/example/itheima/controller/BookController.java
@@ -8,6 +8,7 @@ import com.example.itheima.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -24,13 +25,20 @@ public class BookController {
     }
 
     @PostMapping
-    public R save(@RequestBody Book book) {
-        return new R(bookService.save(book));
+    public R save(@RequestBody Book book) throws IOException {
+//        R r = new R();
+//        boolean flag = bookService.save(book);
+//        r.setFlag(flag);
+        if (book.getName().equals("123") ) throw new IOException();
+        boolean flag = bookService.save(book);
+        return new R(flag, flag ? "添加成功^_^" : "添加失败-_-!");
     }
 
     @PutMapping
-    public R update(@RequestBody Book book) {
-        return new R(bookService.updateById(book));
+    public R update(@RequestBody Book book) throws IOException {
+        if (book.getName().equals("123") ) throw new IOException();
+        boolean flag = bookService.updateById(book);
+        return new R(flag, flag ? "修改成功^_^" : "修改失败-_-!");
     }
 
     @DeleteMapping("{id}")
@@ -45,7 +53,7 @@ public class BookController {
 
     @GetMapping("{currentPage}/{pageSize}")
     public R getPage(@PathVariable int currentPage, @PathVariable int pageSize, Book book) {
-//        System.out.println("参数==>"+book);
+        System.out.println("参数==>"+book);
 
         IPage<Book> page = bookService.getPage(currentPage, pageSize, book);
         //如果当前页码值大于了总页码值，那么重新执行查询操作，使用最大页码值作为当前页码值
